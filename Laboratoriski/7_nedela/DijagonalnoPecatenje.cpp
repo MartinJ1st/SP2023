0f77e1ff700155eff983816f3341b5b792d09bb7
MartinJanev <janev.martin123@gmail.com>
2023-12-13 00:44:22 +0100

    git commit

--- Laboratoriski/7_nedela/DijagonalnoPecatenje.cpp
+++ Laboratoriski/7_nedela/DijagonalnoPecatenje.cpp
@@ -10,7 +10,7 @@ int main()
         }
     }
 
-    for (int k = 0; k < m; ++k) {
+    for (int k = 0; k < n; ++k) {
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 if(i+j==k){
